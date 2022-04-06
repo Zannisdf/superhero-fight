e4675b0df9b29482c977e9e665e29045d454948b
Giorgio Zanni <g.zanni@hotmail.com>
2022-04-06 14:45:46 -0400

    Extract mock team generator to its own file

--- src/fight/__tests__/fight.spec.js
+++ src/fight/__tests__/fight.spec.js
@@ -1,3 +1,4 @@
+const { buildMockTeam } = require('../../../test/helpers/build-mock-team');
 const { buildCreateFight } = require('../fight');
 
 const createFight = buildCreateFight();
@@ -123,26 +124,3 @@ describe('createFight', () => {
     });
   });
 });
-
-function buildMockTeam(overrides = {}) {
-  let heroHP = 100;
-
-  return {
-    performAttack: jest
-      .fn()
-      .mockImplementation(() => ({ attacker: 'attacker', damage: 50 })),
-    takeDamage: jest.fn().mockImplementation((damage) => {
-      heroHP = Math.max(heroHP - damage, 0);
-      return { defender: 'defender', hp: heroHP };
-    }),
-    prepareCurrentFighter: jest.fn(),
-    isCurrentFighterDefeated: jest
-      .fn()
-      .mockReturnValueOnce(false)
-      .mockReturnValueOnce(true),
-    isDefeated: jest.fn().mockReturnValueOnce(false).mockReturnValueOnce(true),
-    switchFighter: jest.fn().mockReturnValueOnce(null),
-    recoverCurrentFighter: jest.fn(),
-    ...overrides,
-  };
-}
