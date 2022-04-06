0e3d9ba7d745194484dcbf57173396cbe1d3e98c
Giorgio Zanni <g.zanni@hotmail.com>
2022-04-06 10:06:55 -0400

    ADD: Secret getter to character service

--- src/services/character/index.js
+++ src/services/character/index.js
@@ -2,6 +2,16 @@ const { createRestClient } = require('../../lib/rest-client');
 const { createCharacterService } = require('./character-service');
 
 const restclient = createRestClient();
-const characterService = createCharacterService({ restclient });
+const characterService = createCharacterService({ restclient, getSecret });
+
+function getSecret() {
+  const secret = process.env.API_KEY;
+
+  if (!secret) {
+    throw new Error('An api key has not been set');
+  }
+
+  return secret;
+}
 
 module.exports = { characterService };
